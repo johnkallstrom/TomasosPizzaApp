bdc2f10baee8f3250194ae678b328993bc851c41
John Källström <jekallstrom@gmail.com>
2020-01-26 23:14:42 +0100

    Add project files.

---
+++ TomasosPizzaApplication/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TomasosPizzaApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TomasosPizzaApplication/Startup.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TomasosPizzaApplication.Models;
+
+namespace TomasosPizzaApplication
+{
+    public class Startup
+    {
+        IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
+            var connection = configuration.GetConnectionString("Default");
+            services.AddDbContext<TomasosContext>(options => options.UseSqlServer(connection));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "Default",
+                template: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
+
+            app.UseStaticFiles();
+        }
+    }
+}
