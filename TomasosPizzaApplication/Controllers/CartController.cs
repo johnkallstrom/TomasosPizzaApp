d143956448b75311e8ac5fdef95b2055c0559f87
John Källström <jekallstrom@gmail.com>
2020-02-07 12:05:30 +0100

    updates.

---
+++ TomasosPizzaApplication/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TomasosPizzaApplication.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddItem(int id)
+        {
+            return View();
+        }
+    }
+}
